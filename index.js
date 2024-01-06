6e791f2c18eee86a22d69503cf9a54380e040d2f
Marco Sosio <marcososio007@gmail.com>
2024-01-07 00:13:59 +0100

    versione 2

--- index.js
+++ index.js
@@ -22,108 +22,109 @@ const combinazioni_vincenti=[
     [0,4,8],[2,4,6]             //diagonali
 ];
 
-function _verifica_vittoria(scelta){
-    for(const combinazione of combinazioni_vincenti){
-        vinto = true;
-        for(const num of combinazione){
-            if(caselle[num].dataset.mossa!=scelta){
-                vinto=false;
+function handler_mossa(){
+
+    const singola_casella=this;
+    let scelta;
+    
+    function esegui_mossa(){
+        turni++;
+
+        let elemento; //cerchio o croce
+        if (turni % 2 == 0) {
+            elemento = singola_casella.children[0]; //cerchio
+            elemento.classList.add("circle-active");
+            scelta = "cerchio";
+        }
+        else {
+            elemento = singola_casella.children[1]; //croce
+            elemento.classList.add("cross-active");
+            scelta = "croce";
+        }
+        singola_casella.dataset.mossa = scelta;
+        // controlla(scelta);
+    }
+
+    function verifica_vittoria() {
+        for (const combinazione of combinazioni_vincenti) {
+            vinto = true;
+            for (const num of combinazione) {
+                if (caselle[num].dataset.mossa != scelta) {
+                    vinto = false;
+                    break;
+                }
+            }
+            if (vinto) {
                 break;
             }
         }
-        if(vinto){
-            break;
-        }
     }
-}
 
-function _fine_gioco(scelta){
-    const testo_dialog = document.getElementById("testo_dialog");
+    function fine_gioco() {
+        const testo_dialog = document.getElementById("testo_dialog");
 
-    function pulisci_tabella(){
-        for (const singola_casella of caselle) {
+        function pulisci_tabella() {
+            for (const singola_casella of caselle) {
 
-            for (const elemento of singola_casella.children) {
-                if (elemento.classList.contains("circle-active")) {
-                    elemento.classList.remove("circle-active");
-                }
-                else if (elemento.classList.contains("cross-active")) {
-                    elemento.classList.remove("cross-active");
+                for (const elemento of singola_casella.children) {
+                    if (elemento.classList.contains("circle-active")) {
+                        elemento.classList.remove("circle-active");
+                    }
+                    else if (elemento.classList.contains("cross-active")) {
+                        elemento.classList.remove("cross-active");
+                    }
                 }
             }
         }
-    }
 
-    function reset(){
-        turni=0;
-        vinto=false;
-        for(const singola_casella of caselle){
-            singola_casella.dataset.mossa="";
-        }
-    }
-
-    function _vittoria(){
-
-        if(scelta=="cerchio"){
-           testo_dialog.innerHTML="Ha vinto il giocatore blu";
-           dialog.classList.add("blue-win");
-        }
-        else if(scelta=="croce"){
-            testo_dialog.innerHTML="Ha vinto il giocatore rosso";
-            dialog.classList.add("red-win");
-        }
-    }
-
-    function pareggio(){
-        testo_dialog.innerHTML="Pareggio";
-        dialog.classList.add("pareggio");
-    }
-
-    if(vinto){
-        _vittoria();
-    }
-    else if(turni==9){
-        pareggio();
-    }
-    pulisci_tabella();
-    reset();
-    dialog.showModal();
-}
-
-function controlla(scelta){
-    _verifica_vittoria(scelta);
-    if(vinto||turni==9){
-        _fine_gioco(scelta);
-    }
-}
-
-function _handler_mossa(){
-    const singola_casella=this;
-    
-    function esegui_mossa(){
-        console.log(singola_casella.dataset.mossa);
-        if (!singola_casella.dataset.mossa) { // "" è false
-            let elemento; //cerchio o croce
-            let scelta;
-            turni++;
-
-            if (turni % 2 == 0) {
-                elemento = singola_casella.children[0]; //cerchio
-                elemento.classList.add("circle-active");
-                scelta = "cerchio"
+        function reset_variabili() {
+            turni = 0;
+            vinto = false;
+            for (const singola_casella of caselle) {
+                singola_casella.dataset.mossa = "";
             }
-            else {
-                elemento = singola_casella.children[1]; //croce
-                elemento.classList.add("cross-active");
-                scelta = "croce";
+        }
+
+        function vittoria() {
+
+            if (scelta == "cerchio") {
+                testo_dialog.innerHTML = "Ha vinto il giocatore blu";
+                dialog.classList.add("blue-win");
             }
-            singola_casella.dataset.mossa = scelta;
-            controlla(scelta);
+            else if (scelta == "croce") {
+                testo_dialog.innerHTML = "Ha vinto il giocatore rosso";
+                dialog.classList.add("red-win");
+            }
+        }
+
+        function pareggio() {
+            testo_dialog.innerHTML = "Pareggio";
+            dialog.classList.add("pareggio");
+        }
+
+        if (vinto) {
+            vittoria();
+        }
+        else if (turni == 9) {
+            pareggio();
+        }
+
+        pulisci_tabella();
+        reset_variabili();
+        dialog.showModal();
+    }
+
+
+    if (!singola_casella.dataset.mossa){
+        esegui_mossa();
+        verifica_vittoria();
+        if (vinto || turni == 9) {
+            fine_gioco();
         }
     }
-    esegui_mossa();
 }
+
 
 for(const singola_casella of caselle){
-    singola_casella.addEventListener("click",_handler_mossa);
+    singola_casella.addEventListener("click",handler_mossa);
 }
